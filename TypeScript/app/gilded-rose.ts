384a317d2a9e40a500ed51100da74311953e5681
manderson-wbd <matthew.anderson@wbdcontractor.com>
2025-04-16 14:26:00 -0400

    first refactoring

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -17,61 +17,73 @@ export class GildedRose {
     this.items = items;
   }
 
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            if (this.items[i].name.startsWith('Conjured')) {
-              this.items[i].quality = this.items[i].quality - 2;
-            } else {
-              this.items[i].quality = this.items[i].quality - 1;
-            }
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
+  private updateNormalItem(item: Item): void {
+    if (item.quality > 0) {
+      item.quality -= 1;
+    }
+    item.sellIn -= 1;
+    if (item.sellIn < 0 && item.quality > 0) {
+      item.quality -= 1;
+    }
+  }
+
+  private updateAgedBrie(item: Item): void {
+    if (item.quality < 50) {
+      item.quality += 1;
+    }
+    item.sellIn -= 1;
+    if (item.sellIn < 0 && item.quality < 50) {
+      item.quality += 1;
+    }
+  }
+
+  private updateBackstagePass(item: Item): void {
+    if (item.quality < 50) {
+      item.quality += 1;
+      if (item.sellIn < 11 && item.quality < 50) {
+        item.quality += 1;
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                if (this.items[i].name.startsWith('Conjured')) {
-                  this.items[i].quality = this.items[i].quality - 2;
-                } else {
-                  this.items[i].quality = this.items[i].quality - 1;
-                }
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
+      if (item.sellIn < 6 && item.quality < 50) {
+        item.quality += 1;
       }
     }
+    item.sellIn -= 1;
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    }
+  }
 
+  private updateConjuredItem(item: Item): void {
+    if (item.quality > 0) {
+      item.quality -= 2;
+    }
+    item.sellIn -= 1;
+    if (item.sellIn < 0 && item.quality > 0) {
+      item.quality -= 2;
+    }
+  }
+
+  updateQuality() {
+    for (const item of this.items) {
+      switch (item.name) {
+        case 'Aged Brie':
+          this.updateAgedBrie(item);
+          break;
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          this.updateBackstagePass(item);
+          break;
+        case 'Sulfuras, Hand of Ragnaros':
+          // Sulfuras does not change
+          break;
+        default:
+          if (item.name.startsWith('Conjured')) {
+            this.updateConjuredItem(item);
+          } else {
+            this.updateNormalItem(item);
+          }
+          break;
+      }
+    }
     return this.items;
   }
 }
